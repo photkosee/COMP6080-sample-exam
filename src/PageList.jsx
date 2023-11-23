0c329314eab3fc95dc5c12bdbc3e0a95b8269b0e
Phot Koseekrainiramon <z5387411@ad.unsw.edu.au>
2023-11-23 16:45:06 +1100

    add: dashboard

--- src/PageList.jsx
+++ src/PageList.jsx
@@ -2,14 +2,15 @@ import React from 'react';
 import { Routes, Route } from 'react-router-dom';
 import Navbar from './components/Navbar';
 import Footer from './components/Footer';
+import Dashboard from './pages/Dashboard';
 
 const PageList = () => {
   return (
     <>
       <Navbar />
       <Routes>
-        {/* <Route path="/" element={<Dashboard />} />
-        <Route path="/blanko" element={<Blanko />} />
+        <Route path="/" element={<Dashboard />} />
+        {/* <Route path="/blanko" element={<Blanko />} />
         <Route path="/slido" element={<Slido />} />
         <Route path="/treto" element={<Treto />} /> */}
       </Routes>
--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -1,9 +1,48 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 
 const Dashboard = () => {
+  const [won, setWon] = useState(0);
+
+  useEffect(() => {
+    if (!localStorage.getItem('won')) {
+      getScore();
+    } else {
+      setWon(() => localStorage.getItem('won'));
+    }
+  }, [won]);
+
+  const getScore = async () => {
+    const response = await fetch('https://cgi.cse.unsw.edu.au/~cs6080/raw/data/info.json');
+    const data = await response.json();
+
+    if (data.error) {
+      alert('fetching error');
+    } else {
+      localStorage.setItem('won', data.score);
+      setWon(data.score);
+    }
+  };
+
   return (
-    <div>
-      
+    <div className='w-screen h-screen flex justify-center items-center'>
+      <div className='flex flex-col justify-center items-center'>
+        <div className='text-red-500 text-[2em]'>
+          Please choose an option from the navbar.
+        </div>
+
+        <div className='flex gap-2'>
+          <div>
+            Games won: {won}
+          </div>
+
+          <button onClick={() => {
+            localStorage.setItem('won', 0);
+            setWon(0);
+          }}>
+            (reset)
+          </button>
+        </div>
+      </div>
     </div>
   );
 };
